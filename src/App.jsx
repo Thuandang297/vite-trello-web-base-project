c38068d7474926b9d0ea85566743345fadc98238
Thuandang297 <Thuandang729@gmail.com>
2024-09-10 23:46:33 +0700

    install mui libary

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import { useState } from 'react'
+import { Button } from '@mui/material'
 import './App.css'
+import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <AccessAlarm/>
+      <ThreeDRotation/>
     </>
   )
 }
