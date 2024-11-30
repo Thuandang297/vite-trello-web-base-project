8026e6bcce5a58d61c740dcdf9bcb3da5590065d
Thuandang297 <Thuandang729@gamil.com>
2024-11-30 20:12:51 +0700

    update

--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -3,7 +3,10 @@ import Column from './Column/Column'
 import Button from '@mui/material/Button'
 import NoteAddIcon from '@mui/icons-material/NoteAdd'
 import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable'
+import { useState } from 'react'
 function ListColumns({ columns }) {
+  const [openCreateColumn, setOpenCreateColumn] = useState(false)
+  const toogleOpenCreateColumn = () => setOpenCreateColumn(!openCreateColumn)
   return (
     <SortableContext items={columns?.map((column => column._id))} strategy={horizontalListSortingStrategy}>
       {/* List Column */}
@@ -18,28 +21,47 @@ function ListColumns({ columns }) {
         {columns?.map(column => (
           < Column key={column?._id} column={column} />
         ))}
-
-        <Box sx={{
-          minWidth: '200px',
-          maxWidth: '200px',
-          margin: 1,
-          borderRadius: '6px',
-          maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`,
-          height: 'fit-content',
-          bgcolor: '#ffffff3d'
-        }}>
-          <Box>
-            <Button
-              sx={{
-                color: 'white',
-                width: '100%',
-                justifyContent: 'flex-start',
-                pl: 2.5,
-                pi: 1
-              }}
-              startIcon={<NoteAddIcon />}> Add new column</Button>
+        {openCreateColumn ?
+          <Box sx={{
+            minWidth: '200px',
+            maxWidth: '200px',
+            margin: 1,
+            borderRadius: '6px',
+            maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`,
+            height: 'fit-content',
+            bgcolor: '#ffffff3d'
+          }}>
+            <Box>
+              <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }} >
+                <TextField id="outlined-search" label="Search..." size='small' type="search" sx={{ minWidth: '120px' }} />
+                <Profiles />
+              </Box>
+            </Box>
+          </Box>:
+          <Box sx={{
+            minWidth: '200px',
+            maxWidth: '200px',
+            margin: 1,
+            borderRadius: '6px',
+            maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`,
+            height: 'fit-content',
+            bgcolor: '#ffffff3d'
+          }}>
+            <Box>
+              <Button
+                sx={{
+                  color: 'white',
+                  width: '100%',
+                  justifyContent: 'flex-start',
+                  pl: 2.5,
+                  pi: 1
+                }}
+                startIcon={<NoteAddIcon />}> Add new column</Button>
+            </Box>
           </Box>
-        </Box>
+        }
+
+
       </Box>
     </SortableContext>
 
