8d27f4e9c84dbb4f86b1b7c5b0dfae0958b64b9c
Thuandang297 <Thuandang729@gmail.com>
2025-03-20 11:55:28 +0700

    update:logout and refresh token

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,23 @@
 import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import ReactDOM from 'react-dom/client'
-import App from '~/App'
-import theme from '~/theme'
-import { store } from './redux/store'
 import { Provider } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import App from '~/App'
+import theme from '~/theme'
+import { injectStore } from '~/utils/authorizeAxios'
+import { store } from './redux/store'
 //Cấu hình react router dom với BrowserRouter
 
 const persistor = persistStore(store)
 
+//Kĩ thuật Inject store là kĩ thuật dùng để import store
+injectStore(store)
+
+import { ConfirmProvider } from 'material-ui-confirm'
 import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename='/'>
@@ -20,7 +25,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <PersistGate loading={null} persistor={persistor} >
         <CssVarsProvider theme={theme}>
           <CssBaseline />
-          <App />
+          <ConfirmProvider>
+            <App />
+          </ConfirmProvider>
           <ToastContainer />
         </CssVarsProvider>
       </PersistGate>
--- src/utils/authorizeAxios.js
+++ src/utils/authorizeAxios.js
@@ -1,11 +1,26 @@
+/* eslint-disable no-unused-vars */
 import axios from 'axios'
+import { StatusCodes } from 'http-status-codes'
 import { toast } from 'react-toastify'
+import { fetchRefreshTokenUserApi } from '~/apis'
+import { fetchLogoutUserApi } from '~/redux/users/userSlice'
 import { interceptorLoadingElements } from './formatter'
-let authorizeAxiosInstance = axios.create()
 
+let authorizeAxiosInstance = axios.create()
 authorizeAxiosInstance.defaults.timeout = 1000 * 60 * 10
 authorizeAxiosInstance.defaults.withCredentials = true
 
+//Không thể import store theo cách thông thường trong file js=> Sử dụng inject store
+let axiosReduxStore
+
+export const injectStore = mainStore => {
+  axiosReduxStore = mainStore
+}
+
+// axiosInstance.interceptors.request.use(config => {
+//   config.headers.authorization = store.getState().auth.token
+//   return config
+// })
 
 //Can thiệp vào giữa các request nhận vào
 authorizeAxiosInstance.interceptors.request.use((config) => {
@@ -16,6 +31,7 @@ authorizeAxiosInstance.interceptors.request.use((config) => {
   return Promise.reject(error)
 })
 
+let requestTokenPromise = null
 // Add a response interceptor
 // Can thiệp vào giữa các response nhận về
 authorizeAxiosInstance.interceptors.response.use((response) => {
@@ -23,29 +39,52 @@ authorizeAxiosInstance.interceptors.response.use((response) => {
   interceptorLoadingElements(false)
   return response
 }, (error) => {
-  interceptorLoadingElements(false)
   let errorMessage = error.message
   if (error?.response?.data?.message) {
     errorMessage = error.response.data.message
   }
-  if (error?.response?.status !== 410) {
-    toast.error(errorMessage, {
-      autoClose: 2000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      theme: 'light'
+  interceptorLoadingElements(false)
+  /*Xử lý việc refresh Token tự động
+  TH1:Khi mã lỗi 401 thì thực hiện logout luôn
+  TH2:Khi mã lỗi là 410 => hết hạn token
+  => Gọi api refresh token
+  => BE trả về accessToken mới
+  => Thực hiện gọi lại request với access token mới vừa truyền vào
+  */
+
+  //originalRequests: Chứa những resquest đang bị gọi lỗi khi hết hạn token
+  const originalRequests = error.config
+  //Trường hợp lỗi trả về là accessToken sai thì mặc định đăng xuất ngay
+  if (error?.response?.status == StatusCodes.UNAUTHORIZED) {
+    axiosReduxStore.dispatch(fetchLogoutUserApi(false))
+  }
+  //Trường hợp hết hạn token
+  else if (error?.response?.status == StatusCodes.GONE && !originalRequests._retry) {
+    originalRequests._retry = true
+    //Check xem gọi Promise refreshToken chưa nếu chưa thì gọi luôn các api lỗi vào Promise này, xử lý như này để đảm bảo mỗi khi accessToken hết hạn nhưng gọi nhiều api 1 lúc
+    // thì chỉ gọi refreshToken 1 lần duy nhất
+    if (!requestTokenPromise) {
+      requestTokenPromise = fetchRefreshTokenUserApi()
+        .then((data) => {
+          //Trả về accessToken sử dụng cho việc gọi lại các api bị lỗi
+          return data?.accessToken
+        })
+        .catch((error) => {
+          //Nếu xuất hiện bất cứ lỗi nào xảy ra khi gọi refreshToken thì đăng xuất luôn
+          // axiosReduxStore.dispatch(fetchLogoutUserApi(false))
+          return Promise.reject(error)
+        })
+        .finally(() => {
+          requestTokenPromise = null
+        })
+    }
+    //B1.Đối với dự án yêu cầu lưu accessToken vào đây thì phải xử lý ở bước này
+
+    //B2.Thực hiện gọi lại các request bị lỗi vì hết hạn token
+    return requestTokenPromise.then((accessToken) => {
+      return authorizeAxiosInstance(originalRequests)
     })
   }
-  toast.error(errorMessage, {
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    theme: 'light'
-  })
   return Promise.reject(error)
 })
 
